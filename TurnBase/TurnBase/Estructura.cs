55ee80d389f26a436260d4a4515edbf71341db05
Francesca Vera <sally.acorn16@hotmail.com>
2026-04-17 15:49:02 -0500

    base de estructura

--- TurnBase/TurnBase/Estructura.cs
+++ TurnBase/TurnBase/Estructura.cs
@@ -10,11 +10,26 @@ namespace TurnBase
     {
         public string Nombre { get; set; }
         public string Tipo { get; set; }
+        public int Vida { get; set; }
 
-        public Estructura(string nombre, string tipo)
+        public Estructura(string nombre, string tipo, int vida)
         {
             Nombre = nombre;
             Tipo = tipo;
+            Vida = vida;
+        }
+
+        public void RecibirDanio(int daño)
+        {
+            Vida = Vida - daño;
+
+            if (Vida < 0)
+            {
+                Vida = 0;
+            }
         }
     }
-}
+
+    // llamar a la clase :
+   // Estructura casa = new Estructura("Casa", "Defensa", 100);
+    //Estructura mina = new Estructura("Mina", "Recoleccion", 60);

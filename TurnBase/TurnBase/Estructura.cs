7d2f71d546755f8a1fc8af9cc31a8dcf392671dd
Francesca Vera <sally.acorn16@hotmail.com>
2026-04-17 15:42:15 -0500

    base de la estructura

---
+++ TurnBase/TurnBase/Estructura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnBase
+{
+    internal class Estructura
+    {
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+
+        public Estructura(string nombre, string tipo)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+        }
+    }
+}

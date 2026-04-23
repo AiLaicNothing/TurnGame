9994ac24f44adee7619e4e9d4c4e849ac82c9a3e
B0adafak <sebastianboada26.1@gmail.com>
2026-04-23 02:54:24 -0500

    Clase Unidad

--- TurnBase/TurnBase/Unidad.cs
+++ TurnBase/TurnBase/Unidad.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace TurnBase
 {
-    internal class Unidad
+    public abstract class Unidad : IObjetivo
     {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        public Unidad(string name, int price, int health, int damage)
+        {
+            Name = name;
+            Price = price;
+            Health = health;
+            Damage = damage;
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            if (Health < 0) Health = 0;
+        }
     }
 }

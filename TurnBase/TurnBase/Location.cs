1a0dc75c9ffba97bf35b9a5a991a2d92e1fd520b
Lord Rum <116317848+LordRumTheThird@users.noreply.github.com>
2026-04-23 15:19:33 -0500

    Nodos
    Created "Location", each has their own list of enemies and structures.
    
    Do replace what's actually need for it to work.
    
    Delete/Replace all that says Temp.

---
+++ TurnBase/TurnBase/Location.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnBase
+{
+    internal class Location
+    {
+        //Please do copy this stuff to the real menu so it works, and replace the temp classes to the real ones.
+        private bool Working = true;
+        new List<TempSoldiers> Soldiers = new List<TempSoldiers>();
+        new List<TempLocation> Locations = new List<TempLocation>();
+        private void CurrentHabitans()
+        {
+            foreach(TempSoldiers Soldiers in this.Soldiers)
+            {
+                Soldiers.data();
+            }
+            foreach(TempLocation Locations  in this.Locations)
+            {
+                Locations.data();
+            }
+        }
+    }
+}
---
+++ TurnBase/TurnBase/TempLocations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnBase
+{
+    internal class TempLocation:TempBase
+    {
+        private string name;
+        private int price;
+        private int hp;
+        private int dmg;
+        private int speed;
+        public TempLocation(string name, int price, int hp, int dmg)
+        {
+            this.name = name;
+            this.price = price;
+            this.hp = hp;
+            this.price = price;
+        }
+        public override void data()
+        {
+            Console.WriteLine("Data:" + name + "," + price + "," + hp + "," + dmg);
+        }
+    }
+}

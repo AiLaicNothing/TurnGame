82e981dc4f66f6a4cefeb27aba2dfd66763eacfe
B0adafak <sebastianboada26.1@gmail.com>
2026-04-23 18:12:44 -0500

    Added Soldado - Tanque - Helicoptero

---
+++ TurnBase/TurnBase/Tamque.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnBase
+{
+    public class Tanque : Unidad
+    {
+        public Tanque() : base("Tank", 100, 200, 40) { }
+
+        public override void Attack(IObjetivo target)
+        {
+            if (target is Soldado || target is Estructura)
+            {
+                Console.WriteLine($"{Name} attacks {target.GetType().Name} for {Damage}");
+                target.TakeDamage(Damage);
+            }
+            else
+            {
+                Console.WriteLine($"{Name} cannot attack {target.GetType().Name}");
+            }
+        }
+    }
+}

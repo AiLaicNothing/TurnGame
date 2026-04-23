bc01cf0a257471e5312c1a961d95055e8c642910
Francesca Vera <sally.acorn16@hotmail.com>
2026-04-23 13:54:05 -0500

    correccíon ortografica

--- TurnBase/TurnBase/Mina.cs
+++ TurnBase/TurnBase/Mina.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace TurnBase
 {
-    class Mina
+    internal class Mina : Estructura
     {
+        public int ProduccionPorTurno { get; set; }
+
+        public Mina(string nombre, int vida)
+            : base(nombre, "Recolectar", vida)
+        {
+            ProduccionPorTurno = 10;
+        }
+
+        // Esto simula lo que hace la mina en cada turno
+        public int ProcesarTurno()
+        {
+            if (Vida <= 0)
+                return 0; // destruida, no produce
+
+            return ProduccionPorTurno;
+        }
     }
 }
+
+//uso:
+//Mina mina = new Mina("Mina de plata", 60);
+
+// cada turno del juego:
+//int dinero = mina.ProcesarTurno();
+//Console.WriteLine($"La mina generó {dinero} de oro");
